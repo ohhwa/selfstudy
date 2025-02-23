94be2be27c56cf23b48fc5d5d8fdcbb1eba51dc0
오현화 Hyunhwa <ohyunwha@gmail.com>
2025-02-23 12:28:07 +0900

    ADD

--- c/Ex04.c
+++ c/Ex04.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
+// 함수 선언, int형이므로 정수형을 반환
 int f(int n) {
-    if (n<=1) return 1;
-    else return n * f(n-1);
+    if (n<=1) return 1;     // n이 1보다 작거나 같으면 true 반환 (반복)
+    else return n * f(n-1); // 그렇지 않으면 n * f(n-1) 반환
 }
 
 int main() {
-    printf("%d", f(7));
+    printf("%d", f(7)); // f함수의 인자에 7 입력
 }
